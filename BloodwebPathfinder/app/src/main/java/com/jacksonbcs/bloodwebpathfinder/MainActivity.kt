d1ca1686ae17ece8b662e9d187b6e98d999ae0db
Jackson Bullard <jsbullar@uark.edu>
2022-11-29 13:52:04 -0600

    Web UI groundwork

--- BloodwebPathfinder/app/src/main/java/com/jacksonbcs/bloodwebpathfinder/MainActivity.kt
+++ BloodwebPathfinder/app/src/main/java/com/jacksonbcs/bloodwebpathfinder/MainActivity.kt
@@ -3,10 +3,17 @@ package com.jacksonbcs.bloodwebpathfinder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            add<WebFragment>(R.id.web_container_view)
+        }
     }
 }
